64cbfa51fe91bd891496b7c98d18469b1793fbd2
Daniel Vidal <danielsv465@hotmail.com>
2022-02-12 10:10:31 -0300

    create dashboard page, add eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,14 @@
-import { AppProps } from "next/app"
+import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from "../styles/theme";
+import { theme } from '../styles/theme';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider theme={ theme }>
-      <Component {...pageProps} />
-    </ChakraProvider>
-    )
-  }
-  
-  export default MyApp
-  
+	return (
+		<ChakraProvider theme={ theme }>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
+}
+
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,37 @@
+import { Button, Flex, Stack } from '@chakra-ui/react';
+import { Input } from '../components/Form/Input';
+
+
 export default function Home() {
-  return (
-    <div><h1>Dashgo</h1></div>
-  )
+	return (
+		<Flex
+			width="100vw"
+			height="100vh"
+			alignItems="center"
+			justifyContent="center"
+		>
+			<Flex
+				as="form"
+				width="100%"
+				maxWidth={360}
+				background="gray.800"
+				p="8"
+				borderRadius={8}
+				flexDirection="column"
+			>
+				<Stack spacing="4">
+					<Input name="email" type="email" label="E-mail" />
+					<Input name="password" type="password" label="Password" />
+				</Stack>
+				<Button
+					type="submit"
+					mt="6"
+					colorScheme="pink"
+					size="lg"
+				>
+          Entrar
+				</Button>
+			</Flex>
+		</Flex>
+	);
 }
